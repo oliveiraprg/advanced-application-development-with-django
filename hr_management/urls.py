7cc2e7321d7eae4dcecce7e74333e79ec811da74
Gabriel Alves Oliveira <111614350+oliveiraprg@users.noreply.github.com>
2022-09-22 20:35:44 -0300

    change in project base template

--- hr_management/urls.py
+++ hr_management/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -7,6 +9,7 @@ urlpatterns = [
     path('companys/', include('companys.urls')),
     path('collaborators/', include('collaborators.urls')),
     path('departments/', include('departments.urls')),
+    path('documents/', include('documents.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
